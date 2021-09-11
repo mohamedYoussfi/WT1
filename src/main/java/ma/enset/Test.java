18c3fc1395ff0aa87dafb06d3d595c589a3252b2
YoussfiENSET <m.youssfi@enset-media.ac.ma>
2021-09-11 21:51:31 +0100

    Comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,6 +1,10 @@
+/**
+ Comment
+ */
 package ma.enset;
 public class Test{
     public static void main(String[]args) {
+        //------------------------------
         System.out.println("Hello!");
     }
 }
