6444cfbf4b7cea41fc287649ccc7a2ff2cf3771e
mohamedYoussfi <med@youssfi.net>
2021-09-11 22:31:27 +0100

    Patch code

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,10 +1,12 @@
 /**
  Comment
+ Line 2 Mohamed ENSET
  */
 package ma.enset;
 public class Test{
     public static void main(String[]args) {
         //------------------------------
-        System.out.println("Hello!");
+        String message="Hello!";
+        System.out.println(message);
     }
 }
